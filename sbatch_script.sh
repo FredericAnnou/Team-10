ea2eaa40810a99e5147a996e9d9895901a4a7a7d
FredericAnnou <123765581+FredericAnnou@users.noreply.github.com>
2023-03-12 11:58:23 +0100

    Update sbatch_script.sh

--- sbatch_script.sh
+++ sbatch_script.sh
@@ -1,10 +1,13 @@
+#!/bin/bash
+  
+
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=28000MB
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=50000MB
 #SBATCH --account=tra23_HpcAiQc
 #SBATCH --partition=g100_usr_prod
-#SBATCH --time=00:05:00
+#SBATCH --time=00:20:00
 #SBATCH --error=job.%j.err
 #SBATCH --output=job.%j.out
 #SBATCH --job-name=nome_job
