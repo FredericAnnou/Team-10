274bdf6e2af0bc1afecd9ed54cac88cb12f64cc5
FredericAnnou <123765581+FredericAnnou@users.noreply.github.com>
2023-03-12 10:07:08 +0100

    Update sbatch_script.sh

--- sbatch_script.sh
+++ sbatch_script.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=1000MB
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=28000MB
 #SBATCH --account=tra23_HpcAiQc
 #SBATCH --partition=g100_usr_prod
 #SBATCH --time=00:05:00
@@ -11,4 +9,8 @@
 #SBATCH --output=job.%j.out
 #SBATCH --job-name=nome_job
 
-ibm.py
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+module load autoload
+python ibm.py
